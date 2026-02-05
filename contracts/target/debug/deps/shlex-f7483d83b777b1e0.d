9e64b41bf27ff30e9f478dcd422adf02b10b4f5b
RobaireTH <robaireth@gmail.com>
2026-02-05 10:16:58 +0000

    Add .gitignore and remove build artifacts
    Co-authored-by: RobaireTH <153534007+RobaireTH@users.noreply.github.com>

--- contracts/target/debug/deps/shlex-f7483d83b777b1e0.d
+++
@@ -1,8 +0,0 @@
-/home/runner/work/ckb-PoP/ckb-PoP/contracts/target/debug/deps/libshlex-f7483d83b777b1e0.rmeta: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs
-
-/home/runner/work/ckb-PoP/ckb-PoP/contracts/target/debug/deps/libshlex-f7483d83b777b1e0.rlib: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs
-
-/home/runner/work/ckb-PoP/ckb-PoP/contracts/target/debug/deps/shlex-f7483d83b777b1e0.d: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/lib.rs:
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/shlex-1.3.0/src/bytes.rs:
--- contracts/target/riscv64imac-unknown-none-elf/debug/deps/ckb_hash-9d5083ae63d037c2.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/ckb-PoP/ckb-PoP/contracts/target/riscv64imac-unknown-none-elf/debug/deps/libckb_hash-9d5083ae63d037c2.rmeta: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ckb-hash-0.119.0/src/lib.rs
-
-/home/runner/work/ckb-PoP/ckb-PoP/contracts/target/riscv64imac-unknown-none-elf/debug/deps/ckb_hash-9d5083ae63d037c2.d: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ckb-hash-0.119.0/src/lib.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ckb-hash-0.119.0/src/lib.rs:
